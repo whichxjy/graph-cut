e30be182122b15001255f92acabdc4a0036956da
whichxjy <whichxjy@gmail.com>
2020-10-12 09:23:05 +0800

    Remove window size

--- main.py
+++ main.py
@@ -5,15 +5,6 @@ if __name__ == "__main__":
     cv2.namedWindow(window_name)
     img = cv2.imread("./hat.jpg")
 
-    screen_res = 1280, 720
-    scale_width = screen_res[0] / img.shape[1]
-    scale_height = screen_res[1] / img.shape[0]
-    scale = min(scale_width, scale_height)
-    window_width = int(img.shape[1] * scale)
-    window_height = int(img.shape[0] * scale)
-
-    cv2.resizeWindow(window_name, window_width, window_height)
-
     cv2.imshow(window_name, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
