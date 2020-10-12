6b36c4ace623b717f79017dd8eec61c232d0ccf7
whichxjy <whichxjy@gmail.com>
2020-10-13 06:18:22 +0800

    Move to Qt

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-import cv2
+import sys
+from cv2 import imread
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import numpy as np
+
+
+class CutGUI(QWidget):
+    def __init__(self):
+        super().__init__()
+
+    def paintEvent(self, event):
+        self.image = imread("./hat.jpg")
+        qimage = QImage(
+            self.image.data, self.image.shape[1], self.image.shape[0], QImage.Format_RGB888).rgbSwapped()
+        painter = QPainter(self)
+        painter.drawPixmap(self.rect(), QPixmap(qimage))
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape:
+            self.close()
+
 
 if __name__ == "__main__":
-    window_name = "Hello World"
-    cv2.namedWindow(window_name)
-    img = cv2.imread("./hat.jpg")
-
-    cv2.imshow(window_name, img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    app = QApplication(sys.argv)
+    gui = CutGUI()
+    gui.show()
+    sys.exit(app.exec_())
