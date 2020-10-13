32e54f345dfed2fdf83a7552a9588b9d88c623a5
whichxjy <whichxjy@gmail.com>
2020-10-13 09:26:45 +0800

    Draw a rectangle

--- main.py
+++ main.py
@@ -14,13 +14,26 @@ class CutGUI(QWidget):
         self.graph_maker = GraphMaker()
         self.display_image = self.graph_maker.image
 
+        self.hello = (0, 0)
+
     def paintEvent(self, event):
         weight = self.display_image.shape[1]
         height = self.display_image.shape[0]
         qimage = QImage(
             self.display_image.data, weight, height, QImage.Format_RGB888).rgbSwapped()
+
         painter = QPainter(self)
         painter.drawPixmap(self.rect(), QPixmap(qimage))
+
+        pen_rectangle = QPen(Qt.red)
+        pen_rectangle.setWidth(3)
+
+        painter.setPen(pen_rectangle)
+        painter.drawRect(self.hello[0], self.hello[1], 3, 3)
+
+    def mouseMoveEvent(self, event):
+        self.hello = (event.x(), event.y())
+        self.update()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
