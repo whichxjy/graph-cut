b6d77a963f55ef55ce9458c9ea12bc74a16e2d05
whichxjy <whichxjy@gmail.com>
2020-10-13 08:56:51 +0800

    Add display image

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ class CutGUI(QWidget):
     def __init__(self):
         super().__init__()
         self.graph_maker = GraphMaker()
+        self.display_image = self.graph_maker.image
 
     def paintEvent(self, event):
-        weight = self.graph_maker.image.shape[1]
-        height = self.graph_maker.image.shape[0]
+        weight = self.display_image.shape[1]
+        height = self.display_image.shape[0]
         qimage = QImage(
-            self.graph_maker.image.data, weight, height, QImage.Format_RGB888).rgbSwapped()
+            self.display_image.data, weight, height, QImage.Format_RGB888).rgbSwapped()
         painter = QPainter(self)
         painter.drawPixmap(self.rect(), QPixmap(qimage))
 
