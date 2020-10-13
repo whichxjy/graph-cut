cd7981b49d577f346e0c04bebc289fd23aac4791
whichxjy <whichxjy@gmail.com>
2020-10-13 08:54:59 +0800

    Init graph maker

---
+++ cut.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+
+class GraphMaker:
+    def __init__(self):
+        self.image = cv2.imread("./resource/hat.jpg")
+        self.graph = np.zeros_like(self.image)
+        self.seed_layer = np.zeros_like(self.image)
--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-import sys
-from cv2 import imread
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+import sys
 import numpy as np
+
+from cut import GraphMaker
 
 
 class CutGUI(QWidget):
     def __init__(self):
         super().__init__()
+        self.graph_maker = GraphMaker()
 
     def paintEvent(self, event):
-        self.image = imread("./hat.jpg")
+        weight = self.graph_maker.image.shape[1]
+        height = self.graph_maker.image.shape[0]
         qimage = QImage(
-            self.image.data, self.image.shape[1], self.image.shape[0], QImage.Format_RGB888).rgbSwapped()
+            self.graph_maker.image.data, weight, height, QImage.Format_RGB888).rgbSwapped()
         painter = QPainter(self)
         painter.drawPixmap(self.rect(), QPixmap(qimage))
 
